ba54e8de2cf767b58260524374cabfa6e6f9fc11
pavelapekhtin <pavel.apekhtin@gmail.com>
2023-09-05 16:55:34 +0200

    chore: toml and json validation implemented

--- dev_scripts/scripts/validators.py
+++ dev_scripts/scripts/validators.py
@@ -1,7 +1,10 @@
+import json
 from typing import Dict, List
 
 import toml
 from pydantic import BaseModel
+
+from scripts.cli.cli_functions import console
 
 
 class MatchCasesModel(BaseModel):
@@ -40,5 +43,20 @@ def validate_match_cases_toml(path: str) -> None:
     try:
         toml_file = toml.load(path)
         TomlSchema.model_validate(toml_file)
+        console.print(
+            f"[green]✔ [/green][yellow]'{path}'[/yellow][green] toml stucture ok."
+        )
+    except Exception as e:
+        raise ValueError(f"Invalid structure of {path} file: {e}")
+
+
+def validate_lang_json(path: str) -> None:
+    try:
+        with open(path, "r") as f:
+            lang_json = json.load(f)
+        LangJsonSchema.model_validate(lang_json)
+        console.print(
+            f"[green]✔ [/green][yellow]'{path}'[/yellow][green] json stucture ok."
+        )
     except Exception as e:
         raise ValueError(f"Invalid structure of {path} file: {e}")
--- dev_scripts/tests/unit/test_validators.py
+++ dev_scripts/tests/unit/test_validators.py
@@ -1,9 +1,9 @@
+import json
 from unittest.mock import mock_open, patch
 
-import pytest
 import toml
 
-from scripts.validators import validate_match_cases_toml
+from scripts.validators import validate_lang_json, validate_match_cases_toml
 
 
 def test_validate_match_cases_toml():
@@ -21,3 +21,28 @@ def test_validate_match_cases_toml():
             "toml.load", return_value=mock_toml_data
         ):  # Mocking toml.load to return mock_toml_data directly
             validate_match_cases_toml("fake_path")
+
+
+def test_validate_lang_json():
+    mock_lang_json = {
+        "name": "Recap",
+        "scopeName": "source.recap",
+        "patterns": [
+            {
+                "name": "constant.numeric",
+                "match": "(?i)\\b(one|two)\\b",
+                "captures": {"1": {"name": "constant.numeric"}},
+            },
+            {
+                "name": "string.quoted",
+                "match": "(?i)\\b(law|order)\\b",
+                "captures": {"1": {"name": "string.quoted"}},
+            },
+        ],
+    }
+
+    with patch("builtins.open", mock_open(read_data=json.dumps(mock_lang_json))):
+        with patch(
+            "json.load", return_value=mock_lang_json
+        ):  # Mocking json.load to return mock_lang_json directly
+            validate_lang_json("fake_path")
